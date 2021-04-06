72aed4d9330a4042d74cbe460a54a4205b2f28cf
Hidayat <hidayetmmdff@gmail.com>
2021-04-06 19:07:00 +0400

    bottom navbar

---
+++ screens/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {View, Text, SafeAreaView} from 'react-native';
+
+const Home = () => {
+  return (
+    <SafeAreaView
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+      <Text>Home</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
