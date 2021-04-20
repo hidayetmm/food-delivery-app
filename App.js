04fb10a4804a9550fac782e4630deb5620b94337
Hidayat <hidayetmmdff@gmail.com>
2021-04-20 21:33:34 +0400

    restaurant page

--- App.js
+++ App.js
@@ -1,21 +1,57 @@
 import React from 'react';
-import {createStackNavigator} from '@react-navigation/stack';
-import {NavigationContainer} from '@react-navigation/native';
-import {Home, OrderDelivery, Restaurant} from './screens';
+import { StatusBar, StyleSheet, View, SafeAreaView } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { Home, OrderDelivery, Restaurant } from './screens';
 import Tabs from './navigation/tabs';
+import { COLORS } from './constants';
+
+const MyStatusBar = ({ backgroundColor, ...props }) => (
+  <View style={[styles.statusBar, { backgroundColor }]}>
+    <SafeAreaView>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </SafeAreaView>
+  </View>
+);
 
 const Stack = createStackNavigator();
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{headerShown: false}}
-        initialRouteName="Home">
-        <Stack.Screen name="Home" component={Tabs} />
-        <Stack.Screen name="Restaurant" component={Restaurant} />
-        <Stack.Screen name="OrderDelivery" component={OrderDelivery} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <>
+      <MyStatusBar
+        backgroundColor={COLORS.lightGray4}
+        barStyle="dark-content"
+      />
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{ headerShown: false }}
+          initialRouteName="Home">
+          <Stack.Screen name="Home" component={Tabs} />
+          <Stack.Screen name="Restaurant" component={Restaurant} />
+          <Stack.Screen name="OrderDelivery" component={OrderDelivery} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 };
+
+const STATUSBAR_HEIGHT = StatusBar.currentHeight;
+const APPBAR_HEIGHT = Platform.OS === 'ios' ? 44 : 56;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  statusBar: {
+    height: STATUSBAR_HEIGHT,
+  },
+  appBar: {
+    backgroundColor: '#79B45D',
+    height: APPBAR_HEIGHT,
+  },
+  content: {
+    flex: 1,
+    backgroundColor: '#33373B',
+  },
+});
 export default App;
--- navigation/tabs.js
+++ navigation/tabs.js
@@ -1,29 +1,30 @@
 import React from 'react';
-import {View, Image, TouchableOpacity} from 'react-native';
+import { View, Image, TouchableOpacity } from 'react-native';
 import {
   createBottomTabNavigator,
   BottomTabBar,
 } from '@react-navigation/bottom-tabs';
-import {Home} from '../screens';
-import {COLORS, icons} from '../constants';
-import Svg, {Path} from 'react-native-svg';
+import { isIphoneX } from 'react-native-iphone-x-helper';
+import { Home } from '../screens';
+import { COLORS, icons } from '../constants';
+import Svg, { Path } from 'react-native-svg';
 const Tab = createBottomTabNavigator();
 
-const TabBarCustomButton = ({accessibilityState, children, onPress}) => {
+const TabBarCustomButton = ({ accessibilityState, children, onPress }) => {
   var isSelected = accessibilityState.selected;
 
   if (isSelected) {
     return (
-      <View style={{flex: 1, alignItems: 'center'}}>
-        <View style={{flexDirection: 'row', position: 'absolute', top: 0}}>
-          <View style={{flex: 1, backgroundColor: COLORS.white}}></View>
+      <View style={{ flex: 1, alignItems: 'center' }}>
+        <View style={{ flexDirection: 'row', position: 'absolute', top: 0 }}>
+          <View style={{ flex: 1, backgroundColor: COLORS.white }}></View>
           <Svg width={75} height={61} viewBox="0 0 75 61">
             <Path
               d="M75.2 0v61H0V0c4.1 0 7.4 3.1 7.9 7.1C10 21.7 22.5 33 37.7 33c15.2 0 27.7-11.3 29.7-25.9.5-4 3.9-7.1 7.9-7.1h-.1z"
               fill={COLORS.white}
             />
           </Svg>
-          <View style={{flex: 1, backgroundColor: COLORS.white}}></View>
+          <View style={{ flex: 1, backgroundColor: COLORS.white }}></View>
         </View>
 
         <TouchableOpacity
@@ -58,7 +59,24 @@ const TabBarCustomButton = ({accessibilityState, children, onPress}) => {
 };
 
 const CustomTabBar = props => {
-  return <BottomTabBar {...props.props} />;
+  if (isIphoneX()) {
+    return (
+      <View>
+        <View
+          style={{
+            position: 'absolute',
+            bottom: 0,
+            left: 0,
+            right: 0,
+            height: 30,
+            backgroundColor: COLORS.white,
+          }}></View>
+        <BottomTabBar {...props.props} />
+      </View>
+    );
+  } else {
+    return <BottomTabBar {...props.props} />;
+  }
 };
 
 const Tabs = () => {
@@ -77,7 +95,7 @@ const Tabs = () => {
         name="Home"
         component={Home}
         options={{
-          tabBarIcon: ({focused}) => (
+          tabBarIcon: ({ focused }) => (
             <Image
               source={icons.cutlery}
               resizeMode="contain"
@@ -95,7 +113,7 @@ const Tabs = () => {
         name="Search"
         component={Home}
         options={{
-          tabBarIcon: ({focused}) => (
+          tabBarIcon: ({ focused }) => (
             <Image
               source={icons.search}
               resizeMode="contain"
@@ -113,7 +131,7 @@ const Tabs = () => {
         name="Like"
         component={Home}
         options={{
-          tabBarIcon: ({focused}) => (
+          tabBarIcon: ({ focused }) => (
             <Image
               source={icons.like}
               resizeMode="contain"
@@ -131,7 +149,7 @@ const Tabs = () => {
         name="User"
         component={Home}
         options={{
-          tabBarIcon: ({focused}) => (
+          tabBarIcon: ({ focused }) => (
             <Image
               source={icons.user}
               resizeMode="contain"
